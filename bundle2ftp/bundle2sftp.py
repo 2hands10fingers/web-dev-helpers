3f8ce3d367a9e98263bc1f3febdb8d1cfd27ce0f
Tono <antonio.nogueras@yahoo.com>
2018-01-12 13:59:55 -0600

    Create bundle2sftp.py

---
+++ bundle2ftp/bundle2sftp.py
@@ -0,0 +1,35 @@
+import pysftp
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Upload the bundle to the Square 205 SFTP assets folder')
+parser.add_argument('-pd','--parentdirectory', type=str, help='the number of pages you wish to scrape')
+parser.add_argument('-chd','--childdirectory', type=str, help='the number of pages you wish to scrape')
+args = parser.parse_args()
+
+# SFTP CREDENTIALS
+host = ''
+pw = ''
+user = ''
+portnum = 2222
+
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None
+bundle_path = f'{os.getcwd()}/{args.parentdirectory}/wordpress/wp-content/themes/{args.childdirectory}/assets/bundle.js'
+remote_path = f'/wp-content/themes/{args.childdirectory}/test/'
+
+
+print('Connecting...')
+with pysftp.Connection(host, username=user, password=pw,
+                       cnopts=cnopts, port=portnum) as sftp:
+
+    print(f"\nUploading Bundle from: \n{bundle_path}")
+
+    try:
+        with sftp.cd(remote_path):
+            print('Connected!')
+            print(f'Placing file in: {remote_path}...')
+            sftp.put(localpath=bundle_path)
+            print('\n\nUpload complete!')
+    except OSError:
+        pass
